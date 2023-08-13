73048df6647d0457a0c7a2361824bfd95853f462
amirmammadov <amirmammado@gmail.com>
2023-08-13 22:53:21 +0400

    staff.js file was edited

--- js/staff.js
+++ js/staff.js
@@ -1,15 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let storedLogData = localStorage.getItem("logData");
+function removeLogDataStorage() {
+  document.addEventListener("DOMContentLoaded", function () {
+    let storedLogData = localStorage.getItem("logData");
 
-  if (storedLogData) {
-    console.log(JSON.parse(storedLogData));
-    localStorage.removeItem("logData");
-  }
-});
+    if (storedLogData) {
+      console.log(JSON.parse(storedLogData));
+      localStorage.removeItem("logData");
+    }
+  });
+}
+removeLogDataStorage();
 
 const itemsContainer = document.querySelector(
   '[data-id="main__content__container"]'
 );
+const nextBtn = document.querySelector("[data-id='next__btn']");
+const alert = document.querySelector("[data-id='alert']");
 
 const staff = [
   {
@@ -26,8 +31,21 @@ const staff = [
   },
 ];
 
+let selectedStaffId = null;
+let selectedStaffName = "";
 let isItemSelected = false;
+
 const selectedStaffData = JSON.parse(localStorage.getItem("staff"));
+
+function dissappearAlert() {
+  setTimeout(() => {
+    alert.style.visibility = "hidden";
+  }, 2000);
+}
+
+function switchBetweenPages(href) {
+  location.href = href;
+}
 
 function displayStaffItems() {
   let displayItem = staff.map((person) => {
@@ -57,54 +75,41 @@ function displayStaffItems() {
   displayItem = displayItem.join("");
   itemsContainer.innerHTML = displayItem;
 }
-
 displayStaffItems();
 
 const items = document.querySelectorAll("[data-id='item']");
-const nextBtn = document.querySelector("[data-id='next__btn']");
-const alert = document.querySelector("[data-id='alert']");
 
-let staffId = null;
-let staffName = "";
+function selectStaffItem() {
+  items.forEach((item) => {
+    item.addEventListener("click", () => {
+      items.forEach((item) => {
+        item.classList.remove("staff--active");
+      });
 
-function dissappearAlert() {
-  setTimeout(() => {
-    alert.style.visibility = "hidden";
-  }, 2000);
-}
+      localStorage.removeItem("service");
 
-function switchBetweenPages(href) {
-  location.href = href;
-}
+      item.classList.add("staff--active");
+      selectedStaffId = Number(item.id);
 
-items.forEach((item) => {
-  item.addEventListener("click", () => {
-    items.forEach((item) => {
-      item.classList.remove("staff--active");
+      staff.forEach((staffItem) => {
+        if (staffItem.id === selectedStaffId) {
+          selectedStaffName = staffItem.name;
+        }
+      });
+
+      isItemSelected = true;
     });
-
-    localStorage.removeItem("service");
-
-    item.classList.add("staff--active");
-    staffId = Number(item.id);
-
-    staff.forEach((staffItem) => {
-      if (staffItem.id === staffId) {
-        staffName = staffItem.name;
-      }
-    });
-
-    isItemSelected = true;
   });
-});
+}
+selectStaffItem();
 
 nextBtn.addEventListener("click", () => {
   if (!isItemSelected) {
     alert.style.visibility = "visible";
     dissappearAlert();
   } else {
-    const selectedStaffArray = [staffId, staffName];
-    if (staffId) {
+    const selectedStaffArray = [selectedStaffId, selectedStaffName];
+    if (selectedStaffId) {
       localStorage.setItem("staff", JSON.stringify(selectedStaffArray));
     }
     switchBetweenPages("../pages/services.html");
